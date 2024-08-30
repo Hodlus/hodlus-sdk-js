e3a1ce9a31d51bdc45c6b1e9d6c763952d0c3263
PABlond <pierre-alexis.blond@live.fr>
2024-08-31 00:14:23 +0200

    basic client implementation

---
+++ client.ts
@@ -0,0 +1,46 @@
+export class Client {
+  _header_token_key = "api-token";
+  baseUrl: string;
+  publicKey: string;
+
+  constructor(
+    publicKey: string,
+    baseUrl: string = "http://10.1.1.43:1338/api"
+  ) {
+    this.baseUrl = baseUrl;
+    this.publicKey = publicKey;
+  }
+
+  async _GET(url: string) {
+    const response = await fetch(`${this.baseUrl}${url}`, {
+      method: "GET",
+      headers: {
+        [this._header_token_key]: this.publicKey,
+      },
+    });
+    const data = await response.json();
+
+    return data;
+  }
+
+  async getStore() {
+    const url = "/store";
+    const data = await this._GET(url);
+
+    return data;
+  }
+
+  async getAllCategories() {
+    const url = "/store/categories";
+    const data = await this._GET(url);
+
+    return data;
+  }
+
+  async getOneCategory(categoryId: string) {
+    const url = `/store/categories/${categoryId}`;
+    const data = await this._GET(url);
+
+    return data;
+  }
+}
