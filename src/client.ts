0cf8763d49d4d041144a335a184f6457c8bfac1e
PABlond <pierre-alexis.blond@live.fr>
2024-09-02 21:57:25 +0200

    add method to fetch carts

--- src/client.ts
+++ src/client.ts
@@ -5,7 +5,7 @@ export class Client {
 
   constructor(
     publicKey: string,
-    baseUrl: string = "http://10.1.1.43:1338/api"
+    baseUrl: string = "http://10.1.1.43:1338/api",
   ) {
     this.baseUrl = baseUrl;
     this.publicKey = publicKey;
@@ -23,38 +23,58 @@ export class Client {
     return data;
   }
 
-  async getStore() {
-    const url = "/store";
-    const data = await this._GET(url);
+  async _POST(url: string, body: Record<string, string | number> = {}) {
+    const response = await fetch(`${this.baseUrl}${url}`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        [this._header_token_key]: this.publicKey,
+      },
+    });
+    const data = await response.json();
 
     return data;
+  }
+
+  async getStore() {
+    const url = "/store";
+
+    return this._GET(url);
   }
 
   async getAllCategories() {
     const url = "/store/categories";
-    const data = await this._GET(url);
 
-    return data;
+    return this._GET(url);
   }
 
   async getOneCategory(categoryId: string) {
     const url = `/store/categories/${categoryId}`;
-    const data = await this._GET(url);
 
-    return data;
+    return this._GET(url);
   }
 
   async getProducts() {
     const url = "/store/categories/products";
-    const data = await this._GET(url);
 
-    return data;
+    return this._GET(url);
   }
 
   async getProductsByCategory(categoryId: string) {
     const url = `/store/categories/${categoryId}/products`;
-    const data = await this._GET(url);
 
-    return data;
+    return this._GET(url);
+  }
+
+  async createOneCart() {
+    const url = `/store/cart`;
+
+    return this._POST(url);
+  }
+
+  async getOneCart(cartId: string) {
+    const url = `/store/cart/${cartId}`;
+
+    return this._GET(url);
   }
 }
